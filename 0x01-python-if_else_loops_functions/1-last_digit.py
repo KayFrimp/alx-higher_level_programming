2963b02d0361a95ffe5883b328fae5c87780ff6d
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-06-06 11:46:51 -0700

    Refactored code to use number % 10 for last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-str_number = str(number)
-last_str_number = str_number[-1]
-last_number = int(last_str_number)
+
 if number < 0:
-    last_number = -last_number
+    last_number = number % -10
+else:
+    last_number = number % 10
+
 print(f"Last digit of {number:d} is {last_number:d}", end=" ")
+
 if last_number > 5:
     print("and is greater than 5")
 elif last_number == 0:
     print("and is 0")
-elif last_number < 6 and last_number != 0:
+else:
     print("and is less than 6 and not 0")
