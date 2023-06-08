f38763bdcc744f769b895fc670cfd6b7b5b1063d
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-06-08 08:15:12 -0700

    Bugfixex

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+def main():
     from add_0 import add
     a = 1
     b = 2
     print("{}".format(a), " + ", "{}".format(b),
           " = ", "{}".format(add(a, b)))
+
+
+if __name__ == "__main__":
+    main()
