97d3535152fb21932320a1011fa194c3564ff99b
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-06-28 10:33:42 -0700

    Corrected ValueError exception message

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -21,6 +21,6 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be an integer")
-
-        self.__size = size
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
