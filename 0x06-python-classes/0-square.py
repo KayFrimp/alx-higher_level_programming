847efffb923acdaaebea224ff6ba3a5dac7423ec
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-06-27 18:34:33 -0700

    BugFix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
+"""Square Class Defined"""
+
+
 class Square:
-    """An empty class that defines a square.
-    """
+    """An empty class that defines a square."""
 
     pass
