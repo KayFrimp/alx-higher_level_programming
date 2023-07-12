41088e361d16996b3cb30ee706a25624ce0e97c5
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-07-12 14:30:55 -0700

    Removed Import statement

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """class_to_json Module"""
-import json
 
 
 def class_to_json(obj):
     """Returns JSON serialization of an object
     """
 
-    return json.dumps(vars(obj))
+    return vars(obj)
