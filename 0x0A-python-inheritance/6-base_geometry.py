4422ac6a66ff575e5ce514e11267f0e905cc93e1
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-07-21 12:24:07 -0700

    Geometry needs improvement

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""BaseGeometry Class module"""
+
+
+class BaseGeometry:
+    """An empty class"""
+
+    def area(self):
+        """Not yet implemented"""
+
+        raise Exception("area() is not implemented")
