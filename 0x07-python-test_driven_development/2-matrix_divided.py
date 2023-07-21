feda02d7158d33ceda3a867ab15a8170fb680973
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-07-21 06:33:27 -0700

    Test cases added

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,20 +11,25 @@ def matrix_divided(matrix, div):
 
     Raises:
         TypeError:
-            1. If matrix is not a list lists of integers or floats.
-            2. If rows in matrix are unequal in size.
-            3. If div is not a number(integer/float).
+            1. If not a matrix
+            2. If matrix is not a list of lists of integers or floats.
+            3. If rows in matrix are unequal in size.
+            4. If div is not a number(integer/float).
         ZeroDivisionError:
             Whenever div is equal to 0.
 
     Returns:
         list: Resulting matrix after division
     """
-    if not matrix or not isinstance(matrix, list) or \
-       not all(isinstance(item, int) or isinstance(item, float) for
+    err_message = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not isinstance(matrix, list) or \
+       not all(isinstance(row, list) for row in matrix):
+        raise TypeError(err_message)
+
+    if not all(isinstance(item, int) or isinstance(item, float) for
                row in matrix for item in row):
-        raise TypeError("matrix must be a matrix (list of lists) "
-                        "of integers/floats")
+        raise TypeError(err_message)
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
