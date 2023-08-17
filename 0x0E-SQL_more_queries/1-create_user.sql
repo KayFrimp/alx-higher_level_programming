f7b18714743b3887575f66499ce7062590e59c65
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-08-17 16:45:43 -0700

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,16 @@
 -- Create user user_0d_1 giving user all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Check if user_0d_1 already exists
+SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' LIMIT 1;
+
+-- If user_0d_1 does not exist, create the user with privileges and password
+INSERT INTO mysql.user (Host, User, Password) VALUES ('localhost', 'user_0d_1', PASSWORD('user_0d_1_pwd'));
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply changes
+FLUSH PRIVILEGES;
+
+-- CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+-- GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
