21782d39b03a180c22b35c2af4ac0fac7aa46fa4
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-08-16 19:54:15 -0700

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Create user user_0d_1 giving user all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+      IDENTIFIED BY 'user_0d_1_pwd';
