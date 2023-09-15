4b8637ec499ee0405111c6e4494772d080d981a0
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-09-15 05:05:22 -0700

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module to select states"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        port=3306,
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    """ create a cvursor object """
+    cursor = db.cursor()
+
+    """ write and execute the sql query """
+    cursor.execute("SELECT * FROM states\
+    WHERE  name LIKE 'N%'ORDER BY states.id ASC")
+
+    """ fetch the results of the query """
+    res = cursor.fetchall()
+
+    """ print the results """
+    for row in res:
+        print(row)
