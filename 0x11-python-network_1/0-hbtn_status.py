d0c6add1ee57e7899753b7219ac2cd5054c1e9ab
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-09-29 06:16:00 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ btn.io/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
-        print(f"\t - body: {type(body)}")
-        print(f"\t - content: {body}")
-        print(f"\t - utf8 content: {body.decode('utf-8')}")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
