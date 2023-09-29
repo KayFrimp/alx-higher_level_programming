6dbdb9d78f8f8614a4b29f2c62a3050464818c12
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-09-29 08:07:17 -0700

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
             body = response.read()
             print(body.decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print(f"Error code: {e}")
+        print(f"Error code: {e.code}")
