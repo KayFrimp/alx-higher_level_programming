0880fdce92cdca515b2c9b9628456e1957ac6b33
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-09-29 06:24:48 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.h\
-btn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
